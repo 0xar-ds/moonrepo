6d504bcd6c6c228b35fab16e829daea97e5baf13
0xar-ds <sadden-rope-sleek@duck.com>
2025-06-13 11:06:06 -0300

    feat: introduce a library on the server scope to define representation/outcomes of api routines

---
+++ libs/server/core/api/src/lib/status-code.ts
@@ -0,0 +1,53 @@
+import { HttpStatus } from '@nestjs/common';
+
+import { AppStatus } from '@~shared/status';
+
+export { AppStatus, HttpStatus };
+
+export interface StatusDescription {
+	code: HttpStatus | AppStatus;
+	message: string;
+}
+
+export class Status {
+	public static SUCCESS: StatusDescription = {
+		code: HttpStatus.OK,
+		message: 'Success.',
+	};
+
+	public static BAD_REQUEST_ERROR: StatusDescription = {
+		code: HttpStatus.BAD_REQUEST,
+		message: 'Bad request.',
+	};
+
+	public static UNAHOTIRZED_ERROR: StatusDescription = {
+		code: HttpStatus.UNAUTHORIZED,
+		message: 'Unauthorized.',
+	};
+
+	public static ACCESS_DENIED_ERROR: StatusDescription = {
+		code: HttpStatus.FORBIDDEN,
+		message: 'Forbidden.',
+	};
+
+	public static SERVICE_UNAVAILABLE = {
+		code: HttpStatus.SERVICE_UNAVAILABLE,
+		message: 'Service accessed or depended-on is not available.',
+	};
+
+	public static UNPROCESSABLE_ENTITY = {
+		code: HttpStatus.UNPROCESSABLE_ENTITY,
+		message:
+			'Entity accessed or specified is not within the schemas of the application.',
+	};
+
+	public static INTERNAL_ERROR: StatusDescription = {
+		code: HttpStatus.INTERNAL_SERVER_ERROR,
+		message: 'Internal error.',
+	};
+
+	public static NOT_FOUND_ERROR: StatusDescription = {
+		code: HttpStatus.NOT_FOUND,
+		message: 'Not found.',
+	};
+}
